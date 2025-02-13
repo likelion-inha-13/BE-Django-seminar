cab35e7bc5050b974eea89673f1c67132d6c78fe
Kihyun <mable0927@gmail.com>
2025-02-14 05:20:37 +0900

    feat : CreateAPI 구현 완료

--- seminar_project/lionapp/urls.py
+++ seminar_project/lionapp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create/',views.create_post)
 ]
--- seminar_project/lionapp/views.py
+++ seminar_project/lionapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json  
+from django.http import JsonResponse  
+from .models import *  
 
-# Create your views here.
+def create_post(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        title = data.get('title')
+        content = data.get('content')
+
+        post = Post(
+            title = title,
+            content = content
+        )
+        post.save()
+        return JsonResponse({'message':'success'})
+    return JsonResponse({'message':'POST 요청만 허용됩니다.'})
