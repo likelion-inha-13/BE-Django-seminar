4edf9dfafb9a82490c99f81e20c814decc1f45cf
Kihyun <mable0927@gmail.com>
2025-02-14 05:13:06 +0900

    feat : posts모델 추가 및 admin 설정 완료

--- seminar_project/lionapp/admin.py
+++ seminar_project/lionapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+admin.site.register(Post) # Post 모델을 admin 페이지에서 관리할 수 있도록 등록
--- seminar_project/lionapp/models.py
+++ seminar_project/lionapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100) # 100글자가 최대인 문자열
+    content = models.TextField() # 글자 수 제한이 없는 긴 문자열
+    create_at = models.DateTimeField(auto_now_add=True) # 처음 Post 생성시, 현재시간 저장
