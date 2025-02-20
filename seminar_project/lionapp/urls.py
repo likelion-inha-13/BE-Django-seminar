ff7eff75fcf87fa90ef672b1d036fdcbec06b554
dasol <5dasoll37@gmail.com>
2025-02-20 14:26:08 +0900

    Docs: lionapp/urls.py 에 url 추가

--- seminar_project/lionapp/urls.py
+++ seminar_project/lionapp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('get/<int:pk>/',views.get_post),
     path('all/',views.get_post_all),
     path('update/<int:pk>/',views.update_post),
-    path('delete/<int:pk>/',views.delete_post)
+    path('delete/<int:pk>/',views.delete_post),
+    path('v2/post',views.create_post_v2), # FBV
+    path('v2/post/<int:pk>',views.PostApiView.as_view()) # CBV
 ]
