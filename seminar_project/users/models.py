bbc304618760cb3a186abce73c4828d2284e7264
dasol <5dasoll37@gmail.com>
2025-02-20 17:08:03 +0900

    Feat: users.User 모델 생성

--- seminar_project/users/models.py
+++ seminar_project/users/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager): # 일반 사용자 & 관리자 계정 생성
+    def create_user(self, email, name, password=None):
+        # 이메일과 이름을 받아 User 객체 생성
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password) # 비밀번호 해싱 처리
+        user.save(using=self._db) # DB에 저장
+        return user
+
+    def create_superuser(self, email, name, password):
+        # 일반 유저 생성 후 is_admin = True로 관리자 계정 생성
+        user = self.create_user(
+            email,
+            name=name,
+            password=password,
+        )
+
+        user.is_admin = True # 관리자 권한 부여
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser): # AbstractBaseUser를 상속하여 User 모델 커스텀
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=100,
+        unique=True, # 이메일은 고유해야 함
+    )
+    name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True) # 계정 활성화 여부
+    is_admin = models.BooleanField(default=False) # 관리자 여부 (처음 생성시 false)
+
+    objects = UserManager() # 기본 UserManager 대신 커스텀 UserManager(우리가 커스텀 한 class) 사용
+
+    USERNAME_FIELD = 'email' # 로그인 시 이메일을 ID로 사용
+    # 기본 User 모델은 username을 사용하지만 우리는 email로 변경
+
+    REQUIRED_FIELDS = ['name'] # 'createsuperuser' 실행 시 입력해야 할 필드
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True # 모든 권한 허용 (추후 수정 가능)
+
+    def has_module_perms(self, app_label):
+        return True # 모든 앱에 접근 가능 (추후 수정 가능)
+
+    @property
+    def is_staff(self): # is_admin이 True이면 is_staff도 True로 설정
+        return self.is_admin
+    
+    class Meta:
+        db_table = 'user' # 테이블명을 user로 설정
