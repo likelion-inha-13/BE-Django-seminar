aebafdbc121718e3912094da3608d792d5d546e0
leechanhyeong <12202049@inha.edu>
2025-02-23 15:21:16 +0900

    feat : models.py 추가

--- seminar_project/lionapp/models.py
+++ seminar_project/lionapp/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100) # 100글자가 최대인 문자열
-    content = models.TextField() # 글자 수 제한이 없는 긴 문자열
+    content = models.TextField(null=True,blank=True) # 글자 수 제한이 없는 긴 문자열
     create_at = models.DateTimeField(auto_now_add=True) # 처음 Post 생성시, 현재시간 저장
-
+    member_id = models.ForeignKey(Member, verbose_name="Member", on_delete=models.CASCADE, related_name="posts") # Member와 일대다 관계설정, Member삭제시 Post도 삭제, 역참조 설정
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     content = models.CharField(max_length = 200, null = True, blank = True) # 100글자가 최대인 문자열, empty값을 null로 저장 
     post_id = models.ForeignKey(Post, verbose_name="Post", on_delete=models.CASCADE, related_name="comments") # Post와 일대다 관계설정, Post삭제시 Comment도 삭제, 역참조 설정
-
+    member_id = models.ForeignKey(Member, verbose_name="Member", on_delete=models.CASCADE, related_name="posts") # Member와 일대다 관계설정, Member삭제시 Comment도 삭제, 역참조 설정
+    
     def __str__(self):
         return self.content #str_method를 통해 각 객체 구분 
+
+
+class Member(models.Model):
+    name = models.CharField(max_length = 20, null = True)
+    email = models.EmailField(max_length=50,unique=True) 
+
+class Userpost(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE )
+    user_id = models.FroeignKey(Member,on_delete=models.CASCADE)
