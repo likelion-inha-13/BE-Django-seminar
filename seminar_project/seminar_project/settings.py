40829640521e07c21d3277c8d87e7f81542b33a7
Kihyun <mable0927@gmail.com>
2025-02-27 06:50:34 +0900

    Chore : 과제 기본 세팅 완료

--- seminar_project/seminar_project/settings.py
+++ seminar_project/seminar_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lionapp', # lionapp을 추가해줍니다
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr' # 언어를 한국어로 변경해줍니다
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul' # 시간대를 서울로 변경해줍니다
 
 USE_I18N = True
 
