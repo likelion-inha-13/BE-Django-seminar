64f7796eba2c061cf4adea048955a967d01f0c7e
minkyeong <haileyre000b@naver.com>
2025-05-27 12:05:04 +0900

    feat: static 관련 설정 추가

--- seminar_project/seminar_project/settings.py
+++ seminar_project/seminar_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import json
 from django.core.exceptions import ImproperlyConfigured
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -201,6 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- seminar_project/seminar_project/urls.py
+++ seminar_project/seminar_project/urls.py
@@ -20,6 +20,8 @@ from django.urls import re_path
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,4 +46,4 @@ urlpatterns = [
     # Swagger url
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
